0b2e0f9518667dbb3058af4c0242f78eddc2f5c2
VI0L3TF0X <matex15243@gmail.com>
2024-05-23 14:23:00 +0200

    prettier

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -3,22 +3,19 @@ import { computed, map } from 'nanostores';
 export const $cart = map<Record<number, CartItem>>({});
 
 export function addToCart(item: ShopItem) {
-	const cartItem = $cart.get()[item.id];
-	const quantity = (cartItem?.quantity ?? 0) + 1;
-	$cart.setKey(item.id, {
-		item,
-		quantity,
-	});
+  const cartItem = $cart.get()[item.id];
+  const quantity = (cartItem?.quantity ?? 0) + 1;
+  $cart.setKey(item.id, {
+    item,
+    quantity,
+  });
 }
 
 export function removeFromCart(itemId: number) {
-	// @ts-ignore
-	$cart.setKey(itemId, undefined);
+  // @ts-ignore
+  $cart.setKey(itemId, undefined);
 }
 
 export const subtotal = computed($cart, (cart) =>
-	Object.values(cart).reduce(
-		(sum, item) => (item ? item.quantity * item.item.price : 0) + sum,
-		0
-	)
+  Object.values(cart).reduce((sum, item) => (item ? item.quantity * item.item.price : 0) + sum, 0)
 );
